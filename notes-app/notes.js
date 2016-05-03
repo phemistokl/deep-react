f64d533ef910a15aad37130b12f76a8621cbec59
Albert Wood <phemistokl@gmail.com>
2016-05-04 00:43:10 +0300

    notes finish

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,7 +2,9 @@ var Note = React.createClass({
     render: function() {
         var style = { backgroundColor: this.props.color };
         return (
-                <div className="note" style={style} > {this.props.children} </div>
+                <div className="note" style={style}>
+                <span className="delete-note" onClick={this.props.onDelete}> x </span>
+                 {this.props.children} </div>
             );
     }
 });
@@ -18,7 +20,7 @@ var NoteEditor = React.createClass({
         this.setState({ text: event.target.value });
     },
 
-    handleNoteAdd: function() {
+    handleNoteAddus: function() {
         var newNote = {
             text: this.state.text,
             color: 'yellow',
@@ -39,7 +41,7 @@ var NoteEditor = React.createClass({
                         value={this.state.text}
                         onChange={this.handleTextChange}
                     />
-                    <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
+                    <button className="add-button" onClick={this.handleNoteAddus}>Add</button>
                 </div>
             );
     }
@@ -64,11 +66,20 @@ var NotesGrid = React.createClass({
     },
 
     render: function() {
+        var onNoteDelete = this.props.onNoteDelete;
+
         return (
             <div className="notes-grid" ref="grid">
                {
                     this.props.notes.map(function(note) {
-                        return <Note key={note.id} color={note.color}> {note.text} </Note>;
+                        return (
+                        <Note 
+                            key={note.id} 
+                            onDelete={onNoteDelete.bind(null, note)}
+                            color={note.color}> 
+                            {note.text} 
+                        </Note>
+                       ); 
                     })
                }
             </div>
@@ -79,33 +90,27 @@ var NotesGrid = React.createClass({
 var NotesApp = React.createClass({
     getInitialState: function() {
         return {
-            notes: [
-                    {
-                        id: 0,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    }, {
-                        id: 1,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    }, {
-                        id: 2,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    }, {
-                        id: 3,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    }, {
-                        id: 4,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    }, {
-                        id: 5,
-                        text: "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона",
-                        color: "#ffd700"
-                    },]
+            notes: []
         };
+    },
+
+    componentDidMount: function() {
+        var localNotes = JSON.parse(localStorage.getItem('notes'));
+        if (localNotes) {
+            this.setState({ notes: localNotes });
+        }
+    },
+
+    componentDidUpdate: function() {
+        this._updateLocalStorage();
+    },
+
+    handleNoteDelete: function(note) {
+        var noteId = note.id;
+        var newNotes = this.state.notes.filter(function(note) {
+            return note.id !== noteId;
+        });
+        this.setState({ notes: newNotes });
     },
 
     handleNoteAdd: function(newNote) {
@@ -119,9 +124,14 @@ var NotesApp = React.createClass({
             <div className="notes-app">
             NotesApp
             <NoteEditor onNoteAdd={this.handleNoteAdd} />
-            <NotesGrid notes={this.state.notes} />
+            <NotesGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete} />
             </div>
             );
+    },
+
+    _updateLocalStorage: function() {
+        var notes = JSON.stringify(this.state.notes);
+        localStorage.setItem('notes', notes);
     }
 });
 
