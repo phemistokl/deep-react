ad413525436c38767e9c817b92f96f03fceb9348
Albert Wood <phemistokl@gmail.com>
2016-05-03 23:08:12 +0300

    about collback

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,23 +1,63 @@
 var Note = React.createClass({
     render: function() {
+        var style = { backgroundColor: this.props.color };
         return (
-                <div className="note"> {this.props.children} </div>
+                <div className="note" style={style} > {this.props.children} </div>
             );
     }
 });
 
 var NoteEditor = React.createClass({
+    getInitialState: function() {
+        return {
+            text: ''
+        };
+    },
+
+    handleTextChange: function(event) {
+        this.setState({ text: event.target.value });
+    },
+
+    handleNoteAdd: function() {
+        var newNote = {
+            text: this.state.text,
+            color: 'yellow',
+            id: Date.now()
+        };
+
+        this.props.onNoteAdd(newNote);
+    },
+
     render: function() {
         return (
-                <div className="note-editor"> NoteEditor </div>
+                <div className="note-editor">
+                    <textarea 
+                        placeholder="Enter your note here..." 
+                        rows={5} 
+                        className="textarea" 
+                        value={this.state.text}
+                        onChange={this.handleTextChange}
+                    />
+                    <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
+                </div>
             );
     }
 });
 
 var NotesGrid = React.createClass({
+    componentDidMount: function() {
+        var grid = this.refs.grid;
+        var msnry = new Masonry( grid, {
+            itemSelector: '.note',
+            columnWidth: 200,
+            gutter: 10,
+            isFitWidth: true
+        });
+    },
+
     render: function() {
         return (
-            <div className="notes-grid">
+            <div className="notes-grid" ref="grid">
                {
                     this.props.notes.map(function(note) {
                         return <Note key={note.id} color={note.color}> {note.text} </Note>;
@@ -60,11 +100,17 @@ var NotesApp = React.createClass({
         };
     },
 
+    handleNoteAdd: function(newNote) {
+        var newNotes = this.state.notes.slice();
+        newNotes.unshift(newNote);
+        this.setState({ notes: newNotes });
+    },
+
     render: function() {
         return (
             <div className="notes-app">
             NotesApp
-            <NoteEditor />
+            <NoteEditor onNoteAdd={this.handleNoteAdd} />
             <NotesGrid notes={this.state.notes} />
             </div>
             );
